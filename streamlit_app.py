00f8c4e4fad82d0259ec7812bfcd45e4148a291a
zaidisf <szaidi@abartacocacola.com>
2025-10-27 12:08:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
 # new section to display nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
     
