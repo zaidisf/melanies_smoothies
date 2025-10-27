5a8bf5cb1fb8f801ff7455decdaf8196b4e5cf74
zaidisf <szaidi@abartacocacola.com>
2025-10-27 12:21:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
 
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_wodth = True)
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
 
     
